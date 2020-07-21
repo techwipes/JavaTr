d4dfbe266f3f84f9066751ccfbe55fd83b24a708
Mitya <techwipes@gmail.com>
2020-07-21 09:43:07 +0500

    added class for finding prime numbers in array

---
+++ src/MathBasic/FindPrime.java
@@ -0,0 +1,16 @@
+package MathBasic;
+
+//simple class that prints prime numbers between [n,m] to the console
+
+public class FindPrime {
+    public static void main(String[] args) {
+    int n = 2;
+    int m = 150;
+    outer:
+    for (int i = n; i < m; i++){
+        if( i % 2 == 0){
+            System.out.println(i);
+        }
+    }
+    }
+}
