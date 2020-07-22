07e3f3e99b8e910c15eb4eb95c5853cdfbcc3100
Mitya <techwipes@gmail.com>
2020-07-22 18:47:39 +0500

    added methods add, subtract, generatorArray, generate => finish

--- src/MathBasic/VectorMultiSize.java
+++ src/MathBasic/VectorMultiSize.java
@@ -9,6 +9,7 @@ public class VectorMultiSize {
     }
 
     // vector's lenght
+
     public double lenght() {
         double sum = 0;
         for (int i = 0; i < values.length; i++) {
@@ -18,17 +19,60 @@ public class VectorMultiSize {
     }
 
     // scalar op.(product)
-    public double scalarOp(VectorMultiSize vector){
+
+    public double scalarOp(VectorMultiSize vector) {
         double result = 0;
 
-        for (int i = 0; i < values.length; i++){
+        for (int i = 0; i < values.length; i++) {
             result += values[i] * vector.values[i];
         }
         return result;
     }
 
     // cos between vectors
-    public double  cos(VectorMultiSize vector){
-        return scalarOp(vector)/(lenght() * vector.lenght());
+
+    public double cos(VectorMultiSize vector) {
+        return scalarOp(vector) / (lenght() * vector.lenght());
+    }
+
+    // summ of vectors
+
+    public VectorMultiSize add(VectorMultiSize vector) {
+        double[] result = new double[values.length];
+
+        for (int i = 0; i < values.length; i++) {
+            result[i] = vector.values[i] + values[i];
+        }
+        return new VectorMultiSize(result);
+    }
+
+    // subtract of vectors
+
+    public VectorMultiSize subtract(VectorMultiSize vector) {
+        double[] result = new double[values.length];
+
+        for (int i = 0; i < values.length; i++) {
+            result[i] = values[i] - vector.values[i];
+        }
+        return new VectorMultiSize(result);
+    }
+
+    // static generator for vector values
+
+    private static double[] generatorRandomArray(int lenght) {
+        double[] array = new double[lenght];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random();
+        }
+        return array;
+    }
+
+    // vector generator
+    public static VectorMultiSize[] generate(int n , int dimension){
+        VectorMultiSize[] vectors = new VectorMultiSize[n];
+        for (int i =0; i<vectors.length; i++){
+            vectors[i]= new VectorMultiSize(generatorRandomArray(dimension));
+        }
+        return vectors;
     }
 }
