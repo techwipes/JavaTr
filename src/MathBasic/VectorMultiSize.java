36e5c6e2be721ac0134c8b430ccdebf37a6cae84
Mitya <techwipes@gmail.com>
2020-07-22 18:32:33 +0500

    added methods scalar op and cos

--- src/MathBasic/VectorMultiSize.java
+++ src/MathBasic/VectorMultiSize.java
@@ -17,5 +17,18 @@ public class VectorMultiSize {
         return Math.sqrt(sum);
     }
 
+    // scalar op.(product)
+    public double scalarOp(VectorMultiSize vector){
+        double result = 0;
 
+        for (int i = 0; i < values.length; i++){
+            result += values[i] * vector.values[i];
+        }
+        return result;
+    }
+
+    // cos between vectors
+    public double  cos(VectorMultiSize vector){
+        return scalarOp(vector)/(lenght() * vector.lenght());
+    }
 }
