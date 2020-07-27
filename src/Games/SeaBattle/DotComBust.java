af20e02b89dcee617ee90ff5143e80e506f903b5
Mitya <techwipes@gmail.com>
2020-07-27 19:40:31 +0500

    first init, added variables, methods

--- src/Games/SeaBattle/DotComBust.java
+++ src/Games/SeaBattle/DotComBust.java
@@ -1,9 +1,58 @@
 package Games.SeaBattle;
 
+
+import java.util.ArrayList;
+
 /**
  * Creating objects DotCom, Getting user "turn"
-
  */
 
 public class DotComBust {
+
+    GameHelper helper = new GameHelper();
+    ArrayList<DotCom> dotComList = new ArrayList<DotCom>();
+    private int numOfGuesses = 0;
+
+    private void setUpGame() {
+        // creating and placing ships
+        DotCom one = new DotCom();
+        one.SetName("Hella");
+        DotCom two = new DotCom();
+        two.SetName("BlackBear");
+        DotCom three = new DotCom();
+        
+    }
+
+    ;
+
+    public void startPlaying() {
+        while (dotComList.isEmpty() != true) {
+            checkUserGuess("UserGuess");
+        }
+    }
+
+    ;
+
+    public String checkUserGuess(String UserGuess) {
+        numOfGuesses++;
+        String result = "Miss";
+        for (DotCom:
+             dotComList) {
+             DotCom.checkYourself();
+
+            return result;
+        }
+    }
+
+    ;
+
+
+}
+
+
+
+
+
+
+
 }
