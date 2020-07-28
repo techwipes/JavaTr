4d334b5ee2a023252789f1786f75a8c62101ad76
Mitya <techwipes@gmail.com>
2020-07-28 16:45:22 +0500

    added setter/getter

--- src/BasicExamples/ClassesAndObjects.java
+++ src/BasicExamples/ClassesAndObjects.java
@@ -5,24 +5,39 @@ import java.util.Arrays;
 public class ClassesAndObjects {
     public static void main(String[] args) {
         Person p1 = new Person();
-        p1.name = "Yog";
-        p1.age = 1442;
+        p1.setName("Yog");
+        p1.setAge(1442);
         p1.speak();
         Person p2 = new Person();
-        p2.name = "Faruh";
-        p2.age = 42;
+        p2.setName("Faruh");
+        p2.setAge(42);
         p2.speak();
-
-
-
 
     }
 }
 
 
 class Person{
-    String name;
-    int age;
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
 
     void speak(){
         System.out.println("My name is " + name + "! I am " + age + " years old!");
