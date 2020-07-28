21aab0d1db55242805fa1cc23db562001a964d17
Mitya <techwipes@gmail.com>
2020-07-28 10:36:04 +0500

    added variables, method getUserInput

--- src/Games/SeaBattle/GameHelper.java
+++ src/Games/SeaBattle/GameHelper.java
@@ -1,9 +1,33 @@
 package Games.SeaBattle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
- * can ефлу user's move, places ships on the field  
+ * can get user's move, places ships on the field
 
  */
 
 public class GameHelper {
+
+    private static final String alphabet = "abcdefg";
+    private int gridLength = 7;
+    private int gridSize = 49;
+    private int[] grid = new int[gridSize];
+    private int comCount = 0;
+
+    public String getUserInput(String prompt){
+        String inputLine = null;
+        System.out.print(prompt + " ");
+        try {
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if (inputLine.length() == 0) return null;
+        } catch (IOException io){
+            System.out.println("IOException: " + io);
+        }
+        return inputLine.toLowerCase();
+    }
+
 }
