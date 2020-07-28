a4e0823171108207c018696c1729d60040962150
Mitya <techwipes@gmail.com>
2020-07-28 10:12:21 +0500

    finished code

--- src/Games/SeaBattle/DotCom.java
+++ src/Games/SeaBattle/DotCom.java
@@ -1,4 +1,7 @@
 package Games.SeaBattle;
+
+import java.util.ArrayList;
+
 /**
  * contains the names and locations of ships, checks the user's move for hitting
 
@@ -6,4 +9,33 @@ package Games.SeaBattle;
 
 
 public class DotCom {
+
+    private ArrayList <String> locationCells;
+    private String name;
+
+    public  void setLocationCells(ArrayList<String> loc){
+        locationCells = loc;
+    }
+
+    public void setName(String n){
+        name = n;
+    }
+
+    public String checkYourSelf(String userInput){
+        String result = "Miss!";
+        int index = locationCells.indexOf(userInput);
+        if ( index >=0){
+            locationCells.remove(index);
+
+            if ( locationCells.isEmpty()){
+                result = "Destroyed!";
+                System.out.println("Wow! You sunk the ship: "  + name);
+            }
+            else{
+                result = "Hit!";
+            }
+        }
+        return result;
+    }
+
 }
