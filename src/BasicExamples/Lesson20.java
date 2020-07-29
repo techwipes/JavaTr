a3215235b3d4d7453f0091062fe2c19beaaca649
Mitya <techwipes@gmail.com>
2020-07-29 16:01:39 +0500

    some example class for final static, finished

---
+++ src/BasicExamples/Lesson20.java
@@ -0,0 +1,15 @@
+package BasicExamples;
+
+public class Lesson20 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Test{
+    public final  int CONST;
+
+    public Test(int CONST){
+        this.CONST = CONST;
+    }
+}
