a468cce61a9fd80f8eaf915ab3f766f0a49540be
Mitya <techwipes@gmail.com>
2020-07-29 09:20:17 +0500

    added constructors

--- src/BasicExamples/Lesson19This.java
+++ src/BasicExamples/Lesson19This.java
@@ -6,13 +6,30 @@ public class Lesson19This {
         h1.setAge(18);
         h1.setName("Ivan");
         h1.getInfo();
+        Human h2 = new Human("Jake");
     }
 }
 
 
-class Human{
+class Human {
     private String name;
     private int age;
+
+    public Human() {
+        System.out.println("This is the first constructor");
+    }
+
+    public Human(String name) {
+        this.name = name;
+        System.out.println("This is the second constructor");
+    }
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+        System.out.println("This is the third constructor");
+    }
+
 
     public String getName() {
         return name;
@@ -30,7 +47,7 @@ class Human{
         this.age = age;
     }
 
-    public void getInfo(){
+    public void getInfo() {
         System.out.println(name + " : " + age);
     }
 }
