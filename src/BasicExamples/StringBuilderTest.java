f3e40da2e153b98ee191f1ae6b10d167953f68ea
Mitya <techwipes@gmail.com>
2020-07-30 10:22:31 +0500

    added formatting examples

--- src/BasicExamples/StringBuilderTest.java
+++ src/BasicExamples/StringBuilderTest.java
@@ -4,10 +4,25 @@ public class StringBuilderTest {
     public static void main(String[] args) {
         StringBuilder sb1 = new StringBuilder("Hello");
         System.out.println(sb1.toString());
-       // sb1.append(" my");
-       // sb1.append(" friend");
+        // sb1.append(" my");
+        // sb1.append(" friend");
         sb1.append(" my").append(" friend");
         System.out.println(sb1);
+
+        String test1 = "123";
+        System.out.printf("String %15s \n", test1);
+        // %s -string  %d - digit  %f - floating point number
+        // \n - next line
+
+        System.out.printf("String %15f \n", 1423.523);
+        System.out.printf("String %-15d \n", 10000);
+        System.out.printf("String %15d \n", 10000);
+
+        System.out.printf("String %.2f \n", 1423.523);
+        System.out.printf("String %.1f \n", 1423.523);
+
+        // % [10]-width  [.2]- n numbers after .
+
 
     }
 }
