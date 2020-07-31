5d42e8e4118bbca77d99be8708cff1922558d2cc
Mitya <techwipes@gmail.com>
2020-07-31 18:59:46 +0500

    base examples about boxing / unboxing

---
+++ src/BasicExamples/WrapperClass.java
@@ -0,0 +1,17 @@
+package BasicExamples;
+
+public class WrapperClass {
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println(x);
+        Integer x2 = 123; // boxing
+        int y = x2;       // unboxing
+
+
+        int z = Integer.parseInt("123");
+
+        // Wrapper classes: Double, Float, Long,
+        // Integer, Short, Byte, Character, Boolean
+    }
+
+}
