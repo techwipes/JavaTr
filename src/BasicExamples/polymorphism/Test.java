3f05781b9fc8fc0e26d84ba349c773a60ec4cb49
Mitya <techwipes@gmail.com>
2020-08-01 11:02:45 +0500

    added examples type casting

--- src/BasicExamples/polymorphism/Test.java
+++ src/BasicExamples/polymorphism/Test.java
@@ -3,19 +3,26 @@ package BasicExamples.polymorphism;
 public class Test {
 
     public static void main(String[] args) {
-       Animal animal = new Animal();
-       Dog dog = new Dog();
-       Cat cat = new Cat();
+        Animal animal = new Animal();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
 
-       testPoly(animal);
-       testPoly(dog);
-       testPoly(cat);
+        testPoly(animal);
+        testPoly(dog);
+        testPoly(cat);
 
+        Dog dog1 = (Dog)getAnimal(dog);
+        dog1.bark();
+        dog1.eat();
 
 
     }
 
-    public static void testPoly(Animal animal){
+    public static void testPoly(Animal animal) {
         animal.eat();
+    }
+
+    public static Animal getAnimal(Animal animal) {
+               return animal;
     }
 }
