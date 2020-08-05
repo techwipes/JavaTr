ce3f5d418f43ee17c53d5a6ae15d388028d3c180
Mitya <techwipes@gmail.com>
2020-08-05 15:06:24 +0500

    testing bitset mask  // found problem with negative hashcode

--- src/IPAddr/IPAddrBitSet.java
+++ src/IPAddr/IPAddrBitSet.java
@@ -1,28 +1,64 @@
 package IPAddr;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.BitSet;
 
+import static IPAddr.PerfomanceTest.bytesToMegabytes;
+
 public class IPAddrBitSet {
-    BitSet map = new BitSet(2^32);
-    String[] data = {"1","2","3","4","5","1","3","4","6"};
-    int[] data2 = {1,2,3};
+    BitSet map = new BitSet(2 ^ 32);
+    String[] data = {"1", "2", "3", "4", "5", "1", "3", "4", "6"};
+    int[] data2 = {1, 2, 3};
 
 }
 
 
-class IPAddrBitSetTest{
-    public static void main(String[] args) {
-        BitSet map = new BitSet(2^32);
-        String[] data = {"192.168.140.1","255.255.255.254","33.3.3.3","4","5","1","3","4","6"};
-        int[] data2 = {1,2,3};
+class IPAddrBitSetTest {
+    public static void main(String[] args) throws IOException {
+        long startTime = System.currentTimeMillis();
+        BitSet bMap = new BitSet((2 ^ 32));
 
-        map.set(Math.abs(data[1].hashCode()));
-        System.out.println(Math.abs(data[1].hashCode()));
-        System.out.println(map.get(910498947));
-
+        InputStream input = new FileInputStream("D:\\trrnts\\ip_addresses\\ip_addresses.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        try {
 
 
+        while (reader.ready()) {
+            String s = reader.readLine();
+            if (bMap.get(Math.abs(s.hashCode())))
+            {
+                continue;
+            }
+            else{
+
+                bMap.set(Math.abs(s.hashCode()));
+            }
+        }
+        reader.close();
+        }
+        catch (IndexOutOfBoundsException i){
+            System.out.println(bMap.cardinality());
+
+        }
+
+
+        // Get the Java runtime
+        Runtime runtime = Runtime.getRuntime();
+        // Run the garbage collector
+        //runtime.gc();
+
+        // Calculate the used memory
+        long memory = runtime.totalMemory() - runtime.freeMemory();
+        System.out.println("Used memory is bytes: " + memory);
+        System.out.println("Used memory is megabytes: "
+                + bytesToMegabytes(memory));
+
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        System.out.println(elapsedTime/6000 + "sec");
+
+        System.out.println(bMap.cardinality());
     }
 }
