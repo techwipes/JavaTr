7812c77aab5030d5977829bba1eda6d44fac0d1b
Mitya <techwipes@gmail.com>
2020-08-13 13:11:47 +0500

    work with Calendar

--- src/BasicExamples/CalendarExmpl/FullMoons.java
+++ src/BasicExamples/CalendarExmpl/FullMoons.java
@@ -1,8 +1,8 @@
 package BasicExamples.CalendarExmpl;
 
-import  static java.lang.System.out;
-import java.util.*;
+import static java.lang.System.out;
 
+import java.util.*;
 
 
 public class FullMoons {
@@ -12,12 +12,12 @@ public class FullMoons {
 
     public static void main(String[] args) {
         Calendar c = Calendar.getInstance();
-        c.set(2004,0,7,15,40);
+        c.set(2004, 0, 7, 15, 40);
         long day1 = c.getTimeInMillis();
         day1 += (DAY_IM * 29.52);
-        for (int x = 0 ; x < 60 ; x ++){
+        for (int x = 0; x < 60; x++) {
             c.setTimeInMillis(day1);
-            out.println(String.format("Fool moon on %tc" , c));
+            out.println(String.format("Fool moon on %tc", c));
             day1 += (DAY_IM * 29.52);
         }
 
