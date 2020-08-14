75cd3a42b008e7d1d48d2475d329d82336e908ae
Mitya <techwipes@gmail.com>
2020-08-14 14:27:40 +0500

    update examples

--- src/BasicExamples/Serialization/ReadObject.java
+++ src/BasicExamples/Serialization/ReadObject.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.Arrays;
 
 public class ReadObject {
     public static void main(String[] args) {
@@ -11,11 +12,19 @@ public class ReadObject {
             FileInputStream fis = new FileInputStream("./data/people.bin");
             ObjectInputStream ois = new ObjectInputStream(fis);
 
-            Person person1 = (Person) ois.readObject();
-            Person person2 = (Person) ois.readObject();
+            /*
+            Считывание объектов из массива
+            int personCount = ois.readInt();
+            Person[] people = new Person[personCount];
 
-            System.out.println(person1);
-            System.out.println(person2);
+            for (int i = 0; i < personCount; i++) {
+                people[i] = (Person) ois.readObject();
+            }
+            */
+
+            // Десериализация массива
+            Person [] people = (Person[]) ois.readObject();
+            System.out.println(Arrays.toString(people));
 
             ois.close();
 
--- src/BasicExamples/Serialization/WriteObject.java
+++ src/BasicExamples/Serialization/WriteObject.java
@@ -9,13 +9,23 @@ public class WriteObject {
     public static void main(String[] args) {
         Person person1 = new Person( 1 ,"Bob");
         Person person2 = new Person( 2 ,"Mike");
+        Person[] people = {new Person( 1 ,"Bob"),new Person( 2 ,"Mike"),new Person( 3 ,"Den")};
 
         try {
             FileOutputStream fos = new FileOutputStream("./data/people.bin");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
 
-            oos.writeObject(person1);
-            oos.writeObject(person2);
+            /*
+            Пример сериализации объектов из массива
+            oos.writeInt(people.length);
+            for(Person person : people){
+                oos.writeObject(person);
+            }
+            */
+
+            // Сериализаци целого массива
+
+            oos.writeObject(people);
 
             oos.close();
 
