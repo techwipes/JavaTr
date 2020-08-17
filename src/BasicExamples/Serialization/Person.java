11624063a3b46f2dcfb7b581d781e2e220e365b1
Mitya <techwipes@gmail.com>
2020-08-17 11:06:36 +0500

    syntax fix

--- src/BasicExamples/Serialization/Person.java
+++ src/BasicExamples/Serialization/Person.java
@@ -3,8 +3,10 @@ package BasicExamples.Serialization;
 import java.io.Serializable;
 
 public class Person implements Serializable {
+    private static final long serialVersionUID = 9008233809921088998L;
     private int id;
     private  String name;
+
 
     public Person (int id, String name){
         this.id = id;
